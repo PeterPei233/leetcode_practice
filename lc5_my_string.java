18b194b6734324ca4e3cc42ca66ad351d13b3d3a
PeterPei <pjspjspjs810@gmail.com>
2018-03-28 23:55:12 +0800

    new leetcode3.28

---
+++ lc5_my_string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        List<Integer> l1 = new LinkedList<>();
+        for(int i =0;i<s.length()-1;i++){
+            if(s.charAt(i)==s.charAt(i+1)) l1.add(i++);
+        }
+        int max = (l1.size()==)
+        while(true){
+            int len1 = l1.size();
+            for(int i = 0;i<len;i++){
+                if(i>0&&i<s.length()-2&&s.charAt(i-1)==s.charAt(i+2)) a.set(i,l1.get(i--));
+                else re
+            }
+        }
+    }
+}
